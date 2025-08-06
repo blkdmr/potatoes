df5cdd3058b84463b55b1ddb12c4df8ab0ed1d74
blkdmr <me@russoalessio.dev>
2025-08-06 12:14:58 +0200

    feat: updated yolo finetuning

--- scripts/finetune_detector.py
+++ scripts/finetune_detector.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 # Load a COCO-pretrained YOLO11n model
-model = YOLO("yolo11n.pt")
-results = model.train(data="../dataset/potatoes-v11/data.yaml", epochs=1, imgsz=640)
+model = YOLO("../models/yolo11n.pt")
+results = model.train(data="../dataset/potatoes-v11/data.yaml", epochs=10, imgsz=640)
